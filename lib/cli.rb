912a90f2ead0688c0967ed007bfa147b85fea53d
patrick <sattelbergerp@gmail.com>
2017-03-05 12:36:26 -0500

    Add colorize support to CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -31,7 +31,7 @@ class CLI
 				month.count.times {|index| printDay(month[index], cmd[0], index+1)}
 			end
 		else
-			puts "Unknown month or command"
+			puts "Unknown month or command".colorize(:light_red)
 		end
 	end
 
@@ -45,7 +45,7 @@ class CLI
 				printDay(day, cmd[0], cmd[1])
 			end
 		else
-			puts "Could not find day in month"
+			puts "Could not find day in month".colorize(:light_red)
 		end
 	end
 
@@ -59,7 +59,7 @@ class CLI
 				printDayDetails(nday, cmd[0], cmd[1])
 			end
 		else
-			puts "Can't find national day on specified date"
+			puts "Can't find national day on specified date".colorize(:light_red)
 		end
 	end
 
@@ -68,7 +68,7 @@ class CLI
 		when "open"
 			Launchy.open(nday.url)
 		else
-			puts "Unknown command for national day"
+			puts "Unknown command for national day".colorize(:light_red)
 		end
 	end
 
@@ -89,9 +89,9 @@ class CLI
 
 	#takes a day of month
 	def printDay(day_of_month, month_name, day_name)
-		puts "#{day_of_month.title}"
+		puts "#{day_of_month.title}".colorize(:light_yellow)
 		day_of_month.days.each_with_index do |nday, index|
-			puts "	#{month_name} #{day_name} #{index+1}. #{nday.name}"
+			puts "	#{month_name} #{day_name} #{index+1}".colorize(:light_blue)+". #{nday.name}"
 		end
 		puts ""
 	end
@@ -99,7 +99,7 @@ class CLI
 	def printDayDetails(national_day, month_name, day_name)
 		national_day.add_details unless national_day.has_details
 		puts "#{national_day.url}"
-		puts "#{national_day.name}"
+		puts "#{national_day.name}".colorize(:light_yellow)
 		puts "#{national_day.summary}"
 	end
 
