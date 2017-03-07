5d26c896086d803a17f85f8157ec96b5c9f20240
Thepattybeast <Thepattybeast@gmail.com>
2017-03-07 11:23:41 -0500

    Implement api, fix api tests

--- lib/nationaldayapi.rb
+++ lib/nationaldayapi.rb
@@ -1,11 +1,41 @@
 class NationalDayApi
 
-	:attr_accessor use_cache
+	attr_accessor :use_cache
 	MONTHS= ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+	URL_BASE = "http://www.nationaldaycalendar.com/"
 
 	def initialize(use_cache=true)
 		@use_cache = use_cache
 		@cache = {}
+	end
+
+	def get_month(input)
+		id = get_month_id(input)
+		month = @cache[id] if use_cache
+		month = DayOfMonth.array_from_hash_array(Scraper.scrape_month("#{URL_BASE}#{id}/")) if @cache[id]==nil || !use_cache
+		@cache[id] = month if use_cache
+		month
+	end
+
+	def cached?(input)
+		@cache[get_month_id(input)]!=nil&&use_cache
+	end
+
+	def clear_cache
+		@cache = {}
+	end
+
+	private
+	def get_month_id(input)
+		index = get_month_index(input)
+		return MONTHS[index] unless index < 0
+		nil
+	end
+
+	def get_month_index(input)
+		return input if input.class==Fixnum
+		return input.to_i()-1 unless input.to_i()==0
+		MONTHS.find_index(input)
 	end
 
 end
--- spec/02_api_spec.rb
+++ spec/02_api_spec.rb
@@ -2,15 +2,16 @@ require_relative '../config/enviroment'
 
 march_7th = NationalDay.new({})
 march_7th.name = "National Cereal Day"
-march_7th.url = "http://www.nationaldaycalendar.com/national-cereal-day-march-7/"
+march_7th.url = "http://nationaldaycalendar.com/national-cereal-day-march-7/"
 
 march_7th_summary = NationalDay.new({})
 march_7th_summary.name = "National Cereal Day"
-march_7th_summary.url = "http://www.nationaldaycalendar.com/national-cereal-day-march-7/"
+march_7th_summary.url = "http://nationaldaycalendar.com/national-cereal-day-march-7/"
 march_7th_summary.summary = "Get your bowl and your spoon ready for March 7 is National Cereal Day. Since the end of the 19th century, cereal has become America’s most popular breakfast food."
 
+api = NationalDayApi.new()
 describe "NationalDayApi" do
-	api = new NationalDayApi()
+
 
 	describe '#get_month' do
 		it 'Returns the corrent month for string "march"' do
@@ -24,29 +25,30 @@ describe "NationalDayApi" do
 		end
 	end
 
-	describe '#is_cached' do
+	describe '#cached?' do
 		it 'Returns the correct cache state for string "march"' do
-			expect(api.is_cached("march")).to eq(true)
+			expect(api.cached?("march")).to eq(true)
 		end
 		it 'Returns the correct cache state for string "3"' do
-			expect(api.is_cached("3")).to eq(true)
+			expect(api.cached?("3")).to eq(true)
 		end
 		it 'Returns the correct cache state for integer 2' do
-			expect(api.is_cached(2)).to eq(true)
+			expect(api.cached?(2)).to eq(true)
 		end
 	end
 
 	describe '#clear_cache' do
 		it 'Clears the cache' do
 			api.clear_cache
-			expect(api.is_cached("march")).to eq(false)
+			expect(api.cached?("march")).to eq(false)
 		end
 	end
 
 	describe '#use_cache' do
 		it 'prevents caching when false' do
+			api.use_cache=false
 			expect(api.get_month("march")[0].title).to eq("March 1st")
-			expect(api.is_cached("march")).to eq(false)
+			expect(api.cached?("march")).to eq(false)
 		end
 	end
 
@@ -58,10 +60,11 @@ describe "NationalDay" do
 		expect(nday).to eq(march_7th)
 	end
 	describe "#add_details" do
-		it "Grabs summary information and sets has_details to true"
+		it "Grabs summary information and sets has_details to true" do
 			expect(nday.has_details).to eq(false)
 			nday.add_details
 			expect(nday.has_details).to eq(true)
 			expect(nday).to eq(march_7th_summary)
 		end
 	end
+end
